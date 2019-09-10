77758a91d3b6c52942fde00f0ef25976194ead59
Sylvain CRISTOFARI <sylvain.cristofari@pa.cotte.fr>
2019-09-10 09:37:04 +0200

    up

--- Makefile
+++ Makefile
@@ -2,6 +2,6 @@ OUT_GO_DIR="."
 
 proto:
 	@echo "generating proto..."
-	@cd protobuf && protoc -I . *.proto --go_out=plugins=grpc:${OUT_GO_DIR}
+	@cd protobuf/grpc-hello && protoc -I . *.proto --go_out=plugins=grpc:${OUT_GO_DIR}
 
 build: proto
--- cmd/grpc/main.go
+++ cmd/grpc/main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"log"
 	"net"
 
+	grcphello "github.com/scristofari/helloproto/protobuf/grpchello"
 	"google.golang.org/grpc"
-
-	"google.golang.org/grpc/examples/helloworld/helloworld"
 	"google.golang.org/grpc/reflection"
 )
 
@@ -16,23 +16,24 @@ var (
 	addr = flag.String("addr", ":50051", "Network host:port to listen on for gRPC connections.")
 )
 
-// server is used to implement helloworld.GreeterServer.
+// server is used to implement grcphello.GreeterServer.
 type server struct{}
 
-// SayHello implements helloworld.GreeterServer
-func (s *server) SayHello(ctx context.Context, in *helloworld.HelloRequest) (*helloworld.HelloReply, error) {
+// SayHello implements grcphello.GreeterServer
+func (s *server) SayHello(ctx context.Context, in *grcphello.HelloRequest) (*grcphello.HelloReply, error) {
 	log.Printf("Handling SayHello request [%v] with context %v", in, ctx)
-	return &helloworld.HelloReply{Message: "Hello " + in.Name}, nil
+	return &grcphello.HelloReply{Message: "Hello " + in.Name}, nil
 }
 
 func main() {
 	flag.Parse()
+	fmt.Println("Listen on: " + *addr)
 	lis, err := net.Listen("tcp", *addr)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
-	helloworld.RegisterGreeterServer(s, &server{})
+	grcphello.RegisterGreeterServer(s, &server{})
 	reflection.Register(s)
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
