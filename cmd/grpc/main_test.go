f981df6daa9201616e57184c1bba0584706e192a
Sylvain CRISTOFARI <sylvain.cristofari@pa.cotte.fr>
2019-09-10 10:31:20 +0200

    up

--- cmd/grpc/main_test.go
+++ cmd/grpc/main_test.go
@@ -5,8 +5,8 @@ import (
 )
 
 func TestSayHello(t *testing.T) {
-	message := "a"
-	expected := "a"
+	message := ""
+	expected := ""
 
 	if message != expected {
 		t.Errorf("failed to say hello, expected %s, got %s", expected, message)
